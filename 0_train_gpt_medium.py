20ff32e64526ace2eb1cad0c2422b757bd4a98a9
snimu <sebastian.nicolas.mueller@gmail.com>
2025-07-12 21:51:17 +0200

    actually print warmup steps to console

--- 0_train_gpt_medium.py
+++ 0_train_gpt_medium.py
@@ -487,7 +487,7 @@ model: nn.Module = torch.compile(model, dynamic=False)
 warmup_steps = 10
 initial_state = copy.deepcopy(dict(model=model.state_dict(), optimizers=[opt.state_dict() for opt in optimizers]))
 for step in range(warmup_steps):
-    print0(f"warmup step:{step}/{warmup_steps}")
+    print0(f"warmup step: {step}/{warmup_steps}", console=True)
     inputs = targets = torch.randint(0, args.vocab_size, size=(args.train_seq_len,), device="cuda")
     model(inputs.to(torch.int32), targets, get_window_size_blocks(0)).backward()
     for param in model.parameters():
